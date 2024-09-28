c067c316c8eca7868ccfe302894915b951c99621
hhaemin <ahrgoals@naver.com>
2024-09-28 22:52:34 +0900

    ResponseEntity 예외처리

--- src/main/java/com/github/supercoding/service/AirReservationService.java
+++ src/main/java/com/github/supercoding/service/AirReservationService.java
@@ -9,6 +9,9 @@ import com.github.supercoding.repository.reservatios.Reservation;
 import com.github.supercoding.repository.reservatios.ReservationRepository;
 import com.github.supercoding.repository.users.UserEntity;
 import com.github.supercoding.repository.users.UserRepository;
+import com.github.supercoding.service.exceptions.InvalidValueException;
+import com.github.supercoding.service.exceptions.NotAcceptException;
+import com.github.supercoding.service.exceptions.NotFoundException;
 import com.github.supercoding.service.mapper.TicketMapper;
 import com.github.supercoding.web.dto.airline.ReservationRequest;
 import com.github.supercoding.web.dto.airline.ReservationResult;
@@ -17,8 +20,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -35,11 +37,21 @@ public class AirReservationService {
         // 1. 유저를 UserId로 가져와서, 선호하는 여행지 도출
         // 2. 선호하는 여행지와 TicketType으로 AirlineTicket table 질의해서 필요한 AirlineTicket
         // 3. 이 둘의 정보를 조합해서 Ticket DTO 만들기
-        UserEntity userEntity = userRepository.findUserById(userId);
+
+        Set<String> ticketTypeSet = new HashSet<>(Arrays.asList("편도", "왕복"));
+
+        if( !ticketTypeSet.contains(ticketType) )
+            throw new InvalidValueException("해당 TicketType " + ticketType + "은 지원하지 않습니다.");
+
+        UserEntity userEntity = userRepository.findUserById(userId).orElseThrow(
+                () -> new NotFoundException("해당 ID: " + userId+ " 유저를 찾을 수 없습니다."));
         String likePlace = userEntity.getLikeTravelPlace();
 
         List<AirlineTicket> airlineTickets
                 = airlineTicketRepository.findAllAirlineTicketWithPlaceAndTicketType(likePlace, ticketType);
+
+        if(airlineTickets.isEmpty())
+            throw new NotFoundException("해당 likePlace: " + likePlace + " 와 TicketType: " + ticketType + "에 해당하는 항공권 찾을 수 없습니다.");
 
         List<Ticket> tickets = airlineTickets.stream().map(TicketMapper.INSTANCE::airlineTicketToTicket).collect(Collectors.toList());
         return tickets;
@@ -54,16 +66,27 @@ public class AirReservationService {
         Integer airlineTicketId = reservationRequest.getAirlineTicketId();
 
         // 1. Passenger Id
-        Passenger passenger = passengerRepository.findPassengerByUserId(userId);
+        Passenger passenger = passengerRepository.findPassengerByUserId(userId)
+                .orElseThrow(() -> new NotFoundException("요청하신 userId " + userId + "에 해당하는 Passenger를 찾을 수 없습니다."));
         Integer passengerId = passenger.getPassengerId();
 
         // 2. price 등의 정보 불러오기
         List<AirlineTicketAndFlightInfo> airlineTicketAndFlightInfo
                 = airlineTicketRepository.findAllAirLineTicketAndFlightInfo(airlineTicketId);
 
+        if(airlineTicketAndFlightInfo.isEmpty())
+            throw new NotFoundException("AirlineTicket Id " + airlineTicketId + " 에 해당하는 항공편과 항공권을 찾을 수 없습니다.");
+
+
         // 3. reservation 생성
+        Boolean isSuccess = false;
+
         Reservation reservation = new Reservation(passengerId, airlineTicketId);
-        Boolean isSuccess = reservationRepository.saveReservation(reservation);
+        try{
+            isSuccess = reservationRepository.saveReservation(reservation);
+        } catch (RuntimeException e) {
+            throw new NotAcceptException("Reservation이 등록되는 과정에 거부되었습니다.");
+        }
 
         // ReservationResult DTO 만들기
         List<Integer> prices = airlineTicketAndFlightInfo.stream().map(AirlineTicketAndFlightInfo::getPrice).collect(Collectors.toList());
--- src/main/java/com/github/supercoding/web/controller/AirReservationController.java
+++ src/main/java/com/github/supercoding/web/controller/AirReservationController.java
@@ -1,17 +1,27 @@
 package com.github.supercoding.web.controller;
 
+import com.github.supercoding.repository.reservatios.Reservation;
 import com.github.supercoding.service.AirReservationService;
+import com.github.supercoding.service.exceptions.InvalidValueException;
+import com.github.supercoding.service.exceptions.NotAcceptException;
+import com.github.supercoding.service.exceptions.NotFoundException;
 import com.github.supercoding.web.dto.airline.ReservationRequest;
 import com.github.supercoding.web.dto.airline.ReservationResult;
 import com.github.supercoding.web.dto.airline.Ticket;
 import com.github.supercoding.web.dto.airline.TicketResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api/air-reservation")
+@Slf4j
 public class AirReservationController {
 
     public final AirReservationService airReservationService;
@@ -20,15 +30,36 @@ public class AirReservationController {
         this.airReservationService = airReservationService;
     }
 
+    @Operation(summary = "선호하는 ticket 탐색")
     @GetMapping("/tickets")
-    public TicketResponse findAirlineTickets(@RequestParam("user-Id") Integer userId,
-                                             @RequestParam("airline-ticket-type") String ticketType){
-
-        List<Ticket> tickets = airReservationService.findUserFavoritePlaceTickets(userId, ticketType);
-        return new TicketResponse(tickets);
+    public ResponseEntity findAirlineTickets(
+            @Parameter(name = "user-Id", description = "유저 ID", example = "1") @RequestParam("userId") Integer userId,
+            @Parameter(name = "airline-ticket-type", description = "항공권 타입", example = "왕복|편도")@RequestParam("airlineTicketType") String ticketType) {
+        try {
+            List<Ticket> tickets = airReservationService.findUserFavoritePlaceTickets(userId, ticketType);
+            TicketResponse ticketResponse = new TicketResponse(tickets);
+            return new ResponseEntity(ticketResponse, HttpStatus.OK);
+        } catch (InvalidValueException ive){
+            log.error("Client 요청에 문제가 있어 다음처럼 출력합니다. " + ive.getMessage());
+            return new ResponseEntity(ive.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (NotFoundException nfe) {
+            log.error("Client 요청이후 DB 검색 중 에러로 다음처럼 출력합니다. " + nfe.getMessage());
+            return new ResponseEntity(nfe.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
+
+    @Operation(summary = "User와 Ticket Id로 예약 진행")
     @PostMapping("/reservations")
-    public ReservationResult makeReservation(@RequestBody ReservationRequest reservationRequest){
-        return airReservationService.makeReservation(reservationRequest);
+    public ResponseEntity makeReservation(@RequestBody ReservationRequest reservationRequest){
+        try{
+            ReservationResult reservationResult = airReservationService.makeReservation(reservationRequest);
+            return new ResponseEntity(reservationResult, HttpStatus.CREATED);
+        } catch (NotFoundException nfe) {
+            log.error("Client 요청이후 DB 검색 중 에러로 다음처럼 출력합니다. " + nfe.getMessage());
+            return new ResponseEntity(nfe.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (NotAcceptException nae) {
+            log.error("Client 요청이 모종의 이유로 거부됩니다." + nae.getMessage());
+            return new ResponseEntity(nae.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+        }
     }
 }
